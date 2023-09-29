4df623e8e54485bbdab41acf0a7961b996511f17
AgafonovKirill <Kirya_agafonov_2002@mail.ru>
2023-09-30 00:38:57 +0500

    Task3

---
+++ Tyuiu.AgafonovKS.Sprint1.Task3.V1.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.AgafonovKS.Sprint1.Task3.V1.Lib;
+
+namespace Tyuiu.AgafonovKS.Sprint1.Task3.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double r = 4;
+            double h = 5;
+            double wait = 251,2;
+            var res = ds.CylinderVolume(r, h);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
